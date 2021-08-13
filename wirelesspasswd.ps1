6b806bd83254b23cfe33acc6d2de80445fa4eb08
sØlØ <84419185+0bfxGH0ST@users.noreply.github.com>
2021-08-13 11:53:22 -0400

    Update wirelesspasswd.ps1

--- wirelesspasswd.ps1
+++ wirelesspasswd.ps1
@@ -2,9 +2,6 @@
 #
 # by 0bfxGH0ST
 # use for testing purposes only
-#
-# source Dr Scripto
-# https://devblogs.microsoft.com/scripting/using-powershell-to-view-and-remove-wireless-profiles-in-windows-10-part-4/
 
 function obtain_wireless_saved_credentials(){
 
